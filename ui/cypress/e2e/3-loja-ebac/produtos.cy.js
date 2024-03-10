60c0255d89725dcd21d55331a3874aad8204caef
EstacioDiFabio <estaciodifabio@hotmail.com>
2024-03-10 11:07:36 -0300

    page objects

--- ui/cypress/e2e/3-loja-ebac/produtos.cy.js
+++ ui/cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="cypress"/>
 
+import produtosPage from '../../support/page-objects/produtos.page';
+
 describe('Funcionalidade: Produtos', () => {
     
     beforeEach(() => {
-        cy.visit('produtos');
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar o primeiro produto da lista', () => {
@@ -38,4 +40,40 @@ describe('Funcionalidade: Produtos', () => {
 
         cy.get('#tab-title-description > a').contains('Descrição');
     });
+
+    it('Deve busar um produto da lista', () => {
+        produtosPage.buscarProdutoLista('Abominable Hoodie');
+    });
+    
+    it('Deve buscar um produto com sucesso', () => {
+        let produtoNome = 'Aether Gym Pant';
+        produtosPage.buscarProduto(produtoNome);
+
+        cy.get('.product_title').should('contain', produtoNome);
+    });
+    
+    it('Deve visitar a página do produto', () => {
+        let produtoNome = 'Aether-Gym-Pant';
+
+        produtosPage.visitarProduto(produtoNome);
+    });
+    
+    it('Deve adicionar produto ao carrinho', () => {
+        let produtoNome = 'Abominable Hoodie';
+        produtosPage.buscarProduto(produtoNome);
+        let quantidade = 5;
+        produtosPage.adicionarProdutoCarrinho('M', 'Green', 5);
+        
+        cy.get('.woocommerce-message').should('contain', `${quantidade} × “${produtoNome}” foram adicionados no seu carrinho.`);
+    }); 
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            let produtoNome = dados[0].nomeProduto;
+            produtosPage.buscarProduto(produtoNome);
+            produtosPage.adicionarProdutoCarrinho(dados[0].tamanho, dados[0].cor, dados[0].quantidade);
+            
+            cy.get('.woocommerce-message').should('contain', produtoNome);
+        });
+    }); 
 });
