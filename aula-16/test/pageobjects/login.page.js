f7e914fc90468919944b9217f7611cb6b01179c2
estaciodifabio <estaciodifabio@hotmail.com>
2024-03-15 23:06:19 -0300

    aula 16

---
+++ aula-16/test/pageobjects/login.page.js
@@ -0,0 +1,28 @@
+import { $ } from '@wdio/globals'
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage {
+   
+    get email() {
+        return $('id:email');
+    }
+   
+    get password() {
+        return $('android=new UiSelector().text("Password")');
+    }
+
+    get btnLogin() {
+        return $('~Login')
+    }
+
+    async login(email, password) {
+        await this.email.setValue(email);
+        await this.password.setValue(password);
+        await this.btnLogin.click();
+
+    }
+}
+
+export default new LoginPage();
---
+++ aula-16/wdio.conf.js
@@ -0,0 +1,34 @@
+export const config = {
+    runner: 'local',
+    port: 4723,
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        // capabilities for local Appium web tests on an Android Emulator
+        platformName: 'Android',
+        browserName: 'Chrome',
+        'appium:deviceName': 'ebac',
+        'appium:platformVersion': '9.0',
+        'appium:automationName': 'UiAutomator2',
+        'appium:app': `${process.cwd()}/app/ebacshop.apks`,
+        'appium:appWaitActivity': '.MainActivity',
+        'appium:disableIdLocatorAutocompletion': true,
+    }],
+    logLevel: 'info',
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: ['appium'],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}
